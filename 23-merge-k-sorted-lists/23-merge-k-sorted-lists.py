69a459178d48b82e528a6d81fb6286b76aaabd00
Harry <43809168+minkukjo@users.noreply.github.com>
2022-07-26 00:00:28 +0900

    Time: 200 ms (34.31%), Space: 18.6 MB (15.72%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -1,18 +1,22 @@
 class ListNode:
-    def __init__(self,val = 0, next = None):
+    def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
 class Solution:
-    def mergeKLists(self,lists:List[ListNode])->ListNode:
-        data = []
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        sorted_list = []
+        
         for list in lists:
             while list:
-                data.append(list.val)
+                sorted_list.append(list.val)
                 list = list.next
-        data = sorted(data)
-        root = temp = ListNode()
-        for i in data:
-            temp.next = ListNode(i)
-            temp = temp.next
-        return root.next
+        
+        sorted_list.sort()
+
+        result = head = ListNode()
+        
+        for val in sorted_list:
+            head.next = ListNode(val)
+            head = head.next
+
+        return result.next

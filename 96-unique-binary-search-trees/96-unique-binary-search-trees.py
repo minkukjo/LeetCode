6cc9d6a6ec80fbbcccb9f165ec7c05c397282827
Harry <43809168+minkukjo@users.noreply.github.com>
2022-09-21 02:04:03 +0900

    Time: 69 ms (5.40%), Space: 13.9 MB (62.41%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numTrees(self, n: int) -> int:
+        dp = [0] * 20
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n+1):
+            total = 0
+            for j in range(0, i+1):
+                left = dp[j]
+                right = dp[i-j-1]
+                total += left * right
+            dp[i] = total
+        return dp[n]

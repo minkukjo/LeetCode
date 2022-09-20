4b2d257aa53d1dff33db38189a636ec270ea194d
Harry <43809168+minkukjo@users.noreply.github.com>
2022-09-21 02:10:54 +0900

    Time: 65 ms (10.00%), Space: 13.9 MB (62.41%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -5,7 +5,7 @@ class Solution:
         dp[1] = 1
         for i in range(2, n+1):
             total = 0
-            for j in range(0, i+1):
+            for j in range(0, i):
                 left = dp[j]
                 right = dp[i-j-1]
                 total += left * right

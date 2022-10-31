7cf8929d8f6bf385796fdf61a2848cb027c399a4
Harry <43809168+minkukjo@users.noreply.github.com>
2022-10-31 23:15:17 +0900

    Time: 291 ms (83.12%), Space: 25.4 MB (76.45%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        if len(nums) < 2:
+            return
+
+        while len(nums) < k:
+            k = k - len(nums)
+
+        l,r = 0, len(nums)-1
+        while l< r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r-1
+        
+        l,r = 0, k-1
+        while l< r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r-1
+        
+        l,r = k, len(nums) -1
+        while l< r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r-1

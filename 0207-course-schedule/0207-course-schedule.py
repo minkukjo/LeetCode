0e18888a2937f36a9c61d353d6557e010bf052ea
Harry <43809168+minkukjo@users.noreply.github.com>
2022-11-18 00:20:16 +0900

    Time: 121 ms (82.12%), Space: 16.7 MB (61.28%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,29 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        def topoSort(V, arr):
-                # Code here
-                n= V
-                ind=[0]*n
-                q=[]
-                for i in range(n):
-                    for j in arr[i]:
-                        ind[j]+=1
-                for i in range(n):
-                    if ind[i]==0:
-                        q.append(i)
-                if  len(q)==0:
+        if not prerequisites:
+            return True
+        
+        def DFS(node):
+            # Cycle detected.
+            if visited[node] == 1:
+                return False
+            
+            # Visit this node, explore neighbors.
+            visited[node] = 1
+            for nbr in G[node]:
+                if visited[nbr] != 2 and not DFS(nbr):
                     return False
-                cnt=0
-                while q:
-                    item=q.pop(0)
-                    cnt+=1
-                    for i in arr[item]:
-                        ind[i]-=1
-                        if ind[i]==0:
-                            q.append(i)       
-                return cnt==numCourses
-        graph = {node: [] for node in range(numCourses)}
-        for seq in prerequisites:
-            preq, course = seq[0], seq[1]
-            graph[course].append(preq)
-        return topoSort(numCourses,graph)
+            
+            # Done visiting node.
+            visited[node] = 2
+            return True
+        
+        # Build the graph.
+        G = collections.defaultdict(list)
+        for postreq, course in prerequisites:
+            G[course].append(postreq)
+            G[postreq]  # Make sure nodes are made for courses with no postreqs.
+        
+        visited = [0] * numCourses
+        for node in G:
+            if not visited[node]:
+                if not DFS(node):
+                    return False
+        return True

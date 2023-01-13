788398d7430daf84cd499fb1c0182ba6ae680847
Harry <43809168+minkukjo@users.noreply.github.com>
2023-01-14 01:10:18 +0900

    Time: 496 ms (98.72%), Space: 36.2 MB (44.82%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,20 +1,25 @@
+from heapq import *
+
 class MedianFinder:
 
     def __init__(self):
-        self.lo = []  
-        self.hi = []  
-
-    def addNum(self, num):
-        heappush(self.lo, -num)             # lo is maxheap, so -1 * num
-        heappush(self.hi, -self.lo[0])      # hi is minheap
-        heappop(self.lo)
+        """
+        initialize your data structure here.
+        """
+        self.min_heap: List[int] = []
+        self.max_heap: List[int] = []
         
-        if len(self.lo) < len(self.hi):
-            heappush(self.lo, -self.hi[0])
-            heappop(self.hi)
-            
-    def findMedian(self):
-        if len(self.lo) > len(self.hi):
-            return -self.lo[0]                  
-        else:
-            return (self.hi[0] - self.lo[0]) / 2  # - as low has -ve values
+
+    def addNum(self, num: int) -> None:
+        heappush(self.max_heap, -heappushpop(self.min_heap, num))
+        
+        if len(self.max_heap) > len(self.min_heap):
+            heappush(self.min_heap, -heappop(self.max_heap))
+        
+
+    def findMedian(self) -> float:
+        has_even_count = len(self.max_heap) == len(self.min_heap)
+
+        if has_even_count:
+            return (-self.max_heap[0] + self.min_heap[0]) / 2.0
+        return float(self.min_heap[0])

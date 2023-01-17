97a872888e9edefa91170f8bc647a877d5cbc8dd
Harry <43809168+minkukjo@users.noreply.github.com>
2023-01-17 21:57:24 +0900

    Time: 3446 ms (5.01%), Space: 14.6 MB (80.32%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t):
+            return ""
+        window, t_count = Counter(), Counter(t)
+        
+        ans = ""
+        last = 0
+        for i, char in enumerate(s):
+            window[char] = window.get(char,0)+1
+            while window & t_count == t_count:
+                if ans =="" or i-last < len(ans):
+                    ans = s[last:i+1]
+                window[s[last]] -= 1
+                last += 1
+        return ans

3320cea543b1e5a1b952a253ce930c51bc759bc3
Harry <43809168+minkukjo@users.noreply.github.com>
2023-01-31 21:37:50 +0900

    Time: 75 ms (60.85%), Space: 15.2 MB (43.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,13 @@
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}  # hashmap, where key is element and value is index
-        for i, num in enumerate(nums):
-            remainder = target - num
-            if remainder in seen:
-                return (seen[remainder], i)
-            seen[num] = i
-        return -1
+        dic = {}
+
+        for i, value in enumerate(nums):
+            dic[value] = i
+        print(dic)
+
+        for i, value in enumerate(nums):
+            if target-value in dic and dic[target-value] != i:
+                return [i, dic[target - value]]
+        

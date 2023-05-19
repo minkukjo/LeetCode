f58eeac173da63fc6bdfb9843ee78d1903247559
Harry <43809168+minkukjo@users.noreply.github.com>
2023-05-19 23:58:58 +0900

    Time: 148 ms (30.49%), Space: 20 MB (7.00%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,12 @@
+class Solution:
+    def trap(self, height):
+        waterLevel = []
+        left = 0
+        for h in height:
+            left = max(left, h) 
+            waterLevel += [left] # over-fill it to left max height
+        right = 0
+        for i, h in reversed(list(enumerate(height))):
+            right = max(right, h)
+            waterLevel[i] = min(waterLevel[i], right) - h # drain to the right height
+        return sum(waterLevel)

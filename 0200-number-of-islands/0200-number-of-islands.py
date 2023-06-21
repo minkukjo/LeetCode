12ddbf69df44a93614aa1affd09d8b73b1f20de6
Harry <43809168+minkukjo@users.noreply.github.com>
2023-06-22 00:24:29 +0900

    Time: 5453 ms (5.03%), Space: 22 MB (38.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,49 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:return 0
-        rows,cols=len(grid),len(grid[0])
-        count,visit=0,set()
-        def bfs(r,c):
-            q=deque()
-            q.append((r,c))
-            visit.add((r,c))
+        visit = []
+        result = 0
+
+        def bfs(i,j):
+            if (i,j) in visit or grid[i][j] == "0":
+                return
+            q = deque()
+            q.append((i,j))
+            visit.append((i,j))
+            
             while q:
-                row,col=q.popleft()
-                direction=[[1,0],[-1,0],[0,1],[0,-1]]
-                for dr,dc in direction:
-                     r,c=row+dr,col+dc
-                     if (r in range(rows) and c in range(cols) and (r,c) not in visit and grid[r][c]=="1"):
-                         q.append((r,c))
-                         visit.add((r,c))
-                     
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c]=="1" and ((r,c) not in visit):
-                    bfs(r,c)
-                    count+=1
-        return count
+                (i,j) = q.pop()
+
+                if i+1 < len(grid) and grid[i+1][j] == "1":
+                    q.append((i+1,j))
+                    grid[i+1][j] = "0"
+                    visit.append((i+1,j))
+
+                
+                if j+1 < len(grid[0]) and grid[i][j+1] == "1":
+                    q.append((i,j+1))
+                    grid[i][j+1] = "0"
+                    visit.append((i,j+1))
+                
+                if i-1 >= 0 and grid[i-1][j] == "1":
+                    q.append((i-1,j))
+                    grid[i-1][j] = "0"
+                    visit.append((i-1,j))
+
+                
+                if j-1 >= 0 and grid[i][j-1] == "1":
+                    q.append((i,j-1))
+                    grid[i][j-1] = "0"
+                    visit.append((i,j-1))
         
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1" and (i,j) not in visit:
+                    bfs(i,j)
+                    result += 1
+        return result
+
+
+
+            
+

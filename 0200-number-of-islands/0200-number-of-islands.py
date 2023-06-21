ca34a6fd920ec31bb61a984c62c835931de3d78a
Harry <43809168+minkukjo@users.noreply.github.com>
2023-06-22 00:22:40 +0900

    Time: 5462 ms (5.03%), Space: 22.2 MB (38.00%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,49 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        visit = []
         result = 0
-        def dfs(grid, x, y):
-            if grid[x][y] != "1":
+        q = []
+        def bfs(i,j):
+            if (i,j) in visit or grid[i][j] == "0":
                 return
             
-            grid[x][y] = "#"
+            q.append((i,j))
+            visit.append((i,j))
+            
+            while q:
+                (i,j) = q.pop()
 
-            if x + 1 < len(grid) and grid[x+1][y] == "1":
-                dfs(grid, x+1, y)
-            if 0 <= x -1 and grid[x-1][y] == "1":
-                dfs(grid, x-1, y)
-            if y + 1 < len(grid[0]) and grid[x][y+1] == "1":
-                dfs(grid, x,y+1)
-            if 0 <= y-1 and grid[x][y-1] == "1":
-                dfs(grid, x,y-1)
+                if i+1 < len(grid) and grid[i+1][j] == "1":
+                    q.append((i+1,j))
+                    grid[i+1][j] = "0"
+                    visit.append((i+1,j))
+
+                
+                if j+1 < len(grid[0]) and grid[i][j+1] == "1":
+                    q.append((i,j+1))
+                    grid[i][j+1] = "0"
+                    visit.append((i,j+1))
+                
+                if i-1 >= 0 and grid[i-1][j] == "1":
+                    q.append((i-1,j))
+                    grid[i-1][j] = "0"
+                    visit.append((i-1,j))
+
+                
+                if j-1 >= 0 and grid[i][j-1] == "1":
+                    q.append((i,j-1))
+                    grid[i][j-1] = "0"
+                    visit.append((i,j-1))
+        
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1":
-                    dfs(grid, i,j)
+                if grid[i][j] == "1" and (i,j) not in visit:
+                    bfs(i,j)
                     result += 1
-
         return result
+
+
+
+            
+

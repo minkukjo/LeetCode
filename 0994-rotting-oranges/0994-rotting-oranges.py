79b797940aeb921aaaa077f9a054caffb14a73d9
Harry <43809168+minkukjo@users.noreply.github.com>
2023-07-06 00:38:06 +0900

    Time: 75 ms (22.64%), Space: 16.3 MB (92.24%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,38 +1,33 @@
+from collections import deque
+
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        dx = [1,-1,0,0]
-        dy = [0,0,1,-1]
-
-        result = 0
-        visit = set()
-        
         q = deque()
+
+        visit = set()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 2:
                     q.append((i,j,0))
-        
-        while q:
-            row,col,time = q.popleft()
-            visit.add((row,col))
-            result = max(result, time)
-            
 
-            for i in range(4):
-                nx = row + dx[i]
-                ny = col + dy[i]
-                if nx < 0 or ny < 0 or nx >= len(grid) or ny >= len(grid[0]):
-                    continue
-                
-                if grid[nx][ny] == 0 or grid[nx][ny] == 2 or (nx,ny) in visit:
-                    continue
-                
-                if grid[nx][ny] == 1:
-                    grid[nx][ny] = 2
-                    q.append((nx,ny,time+1))
+
+        result = 0
+        while q:
+            (i,j,time) = q.popleft()
+
+            result = max(time, result)
+            visit.add((i,j))
+
+            for dr,dc in (1,0), (-1,0), (0,1), (0,-1):
+                cr,cc = i + dr, j+dc
+
+                if 0 <= cr < len(grid) and 0<= cc < len(grid[0]) and grid[cr][cc] == 1 and (cr,cc) not in visit:
+                    grid[cr][cc] = 2
+                    q.append((cr,cc,time+1))
+        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
                     return -1
-        
         return result

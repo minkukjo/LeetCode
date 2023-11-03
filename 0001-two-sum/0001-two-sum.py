6ed8d25355b2ad5adeb982605b36ebd07c5335f5
Harry <43809168+minkukjo@users.noreply.github.com>
2023-11-04 00:28:24 +0900

    Time: 57 ms (92.06%), Space: 17.7 MB (19.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,13 @@
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dic = {}
+        
+        cache = set(nums)
 
-        for i, value in enumerate(nums):
-            dic[value] = i
-        print(dic)
+        for index, num in enumerate(nums):
 
-        for i, value in enumerate(nums):
-            if target-value in dic and dic[target-value] != i:
-                return [i, dic[target - value]]
+            
+            if target - num in cache:
+                found_index = nums.index(target-num)
+                if found_index != index:
+                    return [index, found_index]
         

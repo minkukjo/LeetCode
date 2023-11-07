2dc4b200659cf5ce34830367cbb28e7c4786f1a2
Harry <43809168+minkukjo@users.noreply.github.com>
2023-11-08 00:59:32 +0900

    Time: 48 ms (22.61%), Space: 16.2 MB (54.52%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,3 +1,4 @@
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -5,32 +6,27 @@ class ListNode:
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
-        first = list1
-        second = list2
-        new = ListNode()
-
-        while first != None and second != None:
-
-            val = first.val
-            if first.val <= second.val:
-                first = first.next
-            else:
-                val = second.val
-                second = second.next
-            
-            temp = new
-            while temp.next != None:
-                temp = temp.next
-            temp.next = ListNode(val)
+        new_node = head = ListNode()
         
-        if first != None:
-            temp = new
-            while temp.next != None:
-                temp = temp.next
-            temp.next = first
-        if second != None:
-            temp = new
-            while temp.next != None:
-                temp = temp.next
-            temp.next = second
-        return new.next
+        while list1 and list2:
+            if list1.val < list2.val:
+                temp = ListNode(list1.val)
+                new_node.next = temp
+                list1 = list1.next
+            else:
+                temp = ListNode(list2.val)
+                new_node.next = temp
+                list2 = list2.next
+            new_node = new_node.next
+        
+        while list1:
+            new_node.next = ListNode(list1.val)
+            new_node = new_node.next
+            list1 = list1.next
+        
+        while list2:
+            new_node.next = ListNode(list2.val)
+            new_node = new_node.next
+            list2 = list2.next
+        
+        return head.next

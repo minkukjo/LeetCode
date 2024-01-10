91df223d1549daf30815f0fa713749b3f6dcf83f
Harry <43809168+minkukjo@users.noreply.github.com>
2024-01-10 23:49:51 +0900

    Time: 437 ms (82.72%), Space: 35.4 MB (6.55%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,8 +1,12 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        hset = set()
-        for idx in nums:
-            if idx in hset:
+from collections import Counter
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        c = Counter(nums)
+
+        for value in c:
+            if c[value] > 1:
                 return True
-            else:
-                hset.add(idx)
+        return False

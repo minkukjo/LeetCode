85441970c733c2251b3326ff7a4ed92fb65127ec
Harry <43809168+minkukjo@users.noreply.github.com>
2024-03-27 23:21:40 +0900

    Time: 578 ms (10.83%), Space: 31 MB (38.56%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,14 +1,15 @@
-from collections import deque
-
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = nums[0]
-        current_sum = 0
-        
+
+        answer = nums[0]
+        cur = float("-inf")
         for num in nums:
-            current_sum = current_sum + num
-            max_sum = max(max_sum , current_sum)
-            if current_sum < 0:
-                current_sum = 0
-        return max_sum
+            cur = max(cur + num, num)
+            answer = max(answer, cur)
+        return answer
+
+
+
+
+        

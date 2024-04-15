5e816693e17fc62eeead496e1eacf42b2d529297
Harry <43809168+minkukjo@users.noreply.github.com>
2024-04-16 00:11:11 +0900

    Time: 85 ms (82.65%), Space: 18.2 MB (95.96%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,45 +1,30 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        if not prerequisites:
-            return True
         
-        def dfs(node):
-            if visited[node] == 1:
+        preMap = {i:[] for i in range(numCourses)}
+
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        visitSet = set()
+
+        def dfs(crs):
+            if crs in visitSet:
                 return False
+            if preMap[crs] == []:
+                return True
             
-            visited[node] = 1
-
-            for target in adj[node]:
-                if visited[target] != 2 and not dfs(target):
-                    return False
-            
-            # is it done?
-            visited[node] = 2
-        
+            visitSet.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre): return False
+            visitSet.remove(crs)
+            preMap[crs] = []
             return True
-
-        adj = {}
-        visited = [0] * numCourses
-
-        for i in range(len(prerequisites)):
-            temp = []
-            if prerequisites[i][0] not in adj:
-                temp.append(prerequisites[i][1])
-                adj[prerequisites[i][0]] = temp
-                if prerequisites[i][1] not in adj:
-                    adj[prerequisites[i][1]] = []
-            else:
-                temp.extend(adj[prerequisites[i][0]])
-                temp.append(prerequisites[i][1])
-                adj[prerequisites[i][0]] = temp
-                if prerequisites[i][1] not in adj:
-                    adj[prerequisites[i][1]] = []
-
-
-
-        for node in adj:
-            if not visited[node]:
-                if not dfs(node):
-                    return False
-
+        for crs in range(numCourses):
+            if not dfs(crs): return False
         return True
+        
+
+
+
+

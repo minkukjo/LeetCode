3f0c69e17d0334d7154293b73dab1c742ed160e8
Harry <43809168+minkukjo@users.noreply.github.com>
2024-04-22 22:13:25 +0900

    Time: 840 ms (42.29%), Space: 17 MB (60.91%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,30 +1,16 @@
-from collections import deque
-from typing import List
-
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # queue with current amount and number of coins
-        q = deque([(amount,0)])
-		
-		# set with visited amount to avoid duplicate work
-        visited = set()
+        
+        dp = [float('inf')] * (amount+1)
+        dp[0] = 0
 
-        while q:
-            amount, sum = q.popleft()
-
-            if amount == 0:
-                return sum
-            
+        for i in range(len(dp)):
             for c in coins:
-                new_amount = amount - c
+                if i -c >= 0:
+                    dp[i] = min(dp[i], dp[i-c]+1)
+        return dp[-1] if dp[-1] != float('inf') else -1
 
-                if new_amount in visited or new_amount <0 :
-                    continue
 
-                q.append((new_amount,sum+1))
-                visited.add(new_amount)
-        return -1
 
 
 

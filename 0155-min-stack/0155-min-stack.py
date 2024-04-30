c09b9e02be1cb950b9d1b00b4aa7835bb7b73c63
Harry <43809168+minkukjo@users.noreply.github.com>
2024-05-01 02:34:12 +0900

    Time: 60 ms (30.17%), Space: 20.5 MB (55.76%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,12 +1,15 @@
+from heapq import heappush, heappop
+
 class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.min = []
+        self.min = [float('inf')]
 
     def push(self, val: int) -> None:
         self.stack.append(val)
         self.min.append(val if not self.min else min(self.min[-1], val))
+
     def pop(self) -> None:
         self.stack.pop()
         self.min.pop()
@@ -16,3 +19,12 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.min[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()

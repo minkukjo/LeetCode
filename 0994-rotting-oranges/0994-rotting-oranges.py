10f76d60e032e0aa580e2d252ff2fddddf5b25a8
Harry <43809168+minkukjo@users.noreply.github.com>
2024-05-08 22:17:18 +0900

    Time: 47 ms (76.56%), Space: 16.7 MB (24.84%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -3,31 +3,42 @@ from collections import deque
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        
+        m = len(grid)
+        n = len(grid[0])
+
         q = deque()
 
-        visit = set()
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(m):
+            for j in range(n):
+
                 if grid[i][j] == 2:
-                    q.append((i,j,0))
-
-
-        result = 0
-        while q:
-            (i,j,time) = q.popleft()
-
-            result = max(time, result)
-            visit.add((i,j))
-
-            for dr,dc in (1,0), (-1,0), (0,1), (0,-1):
-                cr,cc = i + dr, j+dc
-
-                if 0 <= cr < len(grid) and 0<= cc < len(grid[0]) and grid[cr][cc] == 1 and (cr,cc) not in visit:
-                    grid[cr][cc] = 2
-                    q.append((cr,cc,time+1))
+                    q.append((i,j, 0))
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        d = [(0,1), (0,-1), (1,0) ,(-1,0)]
+
+        ans = 0
+        while q:
+            
+            x,y, days = q.popleft()
+            ans = max(ans, days)
+
+            for dx, dy in d:
+                nx = dx + x
+                ny = dy + y
+
+                if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1:
+                    grid[nx][ny] = 2
+                    q.append((nx,ny,days+1))
+
+        for i in range(m):
+            for j in range(n):
+
                 if grid[i][j] == 1:
                     return -1
-        return result
+        
+        return ans
+
+
+
+            

7fc0af2d5a65013bac8acb8a1d4941828d268fee
Harry <43809168+minkukjo@users.noreply.github.com>
2024-05-20 19:21:12 +0900

    Time: 45 ms (25.75%), Space: 16.8 MB (10.92%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,23 +1,19 @@
-from typing import List
-
-
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
+        n = len(nums)
         ans = []
-        def dfs(cur, cur_len):
-            if cur_len == len(nums):
-                if cur not in ans:
-                    ans.append(cur)
+        def dfs(cur):
+
+            if len(cur) == n:
+                ans.append(cur)
                 return
             
             for num in nums:
                 if num not in cur:
-                    dfs(cur + [num], cur_len+1)
+                    dfs(cur + [num])
 
         for num in nums:
-            dfs([num], 1)
-        return ans
+            dfs([num])
 
-s = Solution()
-s.permute([1,2,3])
+        return ans

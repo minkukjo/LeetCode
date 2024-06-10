9ca0ae24957087d32c0af4ed328f567bc211f915
Harry <43809168+minkukjo@users.noreply.github.com>
2024-06-11 01:30:50 +0900

    Time: 35 ms (56.48%), Space: 16.7 MB (15.03%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,24 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        
+        visit = []
+        ans = []
+        def dfs(x,y):
+            if (x,y) in visit:
+                return
+            if (0 <= x < len(matrix) and 0 <= y < len(matrix[0])) is False:
+                return
+
+            visit.append((x,y))
+            ans.append(matrix[x][y])
+
+            if y+1 >= x:
+                dfs(x,y+1)
+            dfs(x+1,y)
+            dfs(x,y-1)
+            dfs(x-1,y)
+            
+        dfs(0,0)
+        
+        return ans
+

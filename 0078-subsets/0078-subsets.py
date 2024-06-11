30b28d826cc38fc367bc51aa84fb8d8075f9399d
Harry <43809168+minkukjo@users.noreply.github.com>
2024-06-11 23:02:59 +0900

    Time: 26 ms (98.30%), Space: 16.6 MB (73.66%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,16 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
         ans = []
-        nums.sort()
-        
-        self.dfs(ans,0,[],nums)
+
+        def dfs(arr, i):
+            if i == len(nums):
+                ans.append(list(arr))
+                return
+            
+            arr.append(nums[i])
+            dfs(arr, i+1)
+            arr.pop()
+            dfs(arr,i+1)
+
+        dfs([],0)
         return ans
-    def dfs(self, ans, start, sub, nums):
-        ans.append(list(sub))
-        for i in range(start, len(nums)):
-            sub.append(nums[i])
-            self.dfs(ans, i+1, sub, nums)
-            sub.pop()
-
-

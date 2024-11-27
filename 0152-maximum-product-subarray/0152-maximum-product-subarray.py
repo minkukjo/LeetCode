f5826d7d6c33229711da920dbbe1e9f3bd012c9c
Harry <43809168+minkukjo@users.noreply.github.com>
2024-11-27 21:52:09 +0900

    Time: 11 ms (48.78%), Space: 17.8 MB (10.85%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,16 +1,14 @@
-from collections import deque
-
-
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        max1 = nums[0]
+        
         min1 = nums[0]
+        max1 = nums[0]
 
         result = nums[0]
 
-        for i in range(1, len(nums)):
+        for i in range(1,len(nums)):
             temp = max1
-            max1 = max(nums[i] * max1, nums[i] * min1, nums[i])
-            min1 = min(temp*nums[i], nums[i]* min1, nums[i])
-            result = max(result, max1)
+            max1 = max(nums[i], nums[i]*max1, nums[i]*min1)
+            min1 = min(nums[i], nums[i]*temp, nums[i]*min1)
+            result = max(max1,result)
         return result

b4a775b3225a54140d40534e518c3983f2b08686
Harry <43809168+minkukjo@users.noreply.github.com>
2024-12-16 20:39:23 +0900

    Time: 3 ms (70.03%), Space: 18.4 MB (5.07%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,18 +1,41 @@
+class Node:
+    def __init__(self):
+        self.map = {}
+        self.isEnd = False
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+        self.failed = [False] * 301
+
+    def insert(self, word):
+        node = self.root
+        for ch in word:
+            if ch not in node.map:
+                node.map[ch] = Node()
+            node = node.map[ch]
+        node.isEnd = True
+    
+    def search(self, s, start):
+        if self.failed[start]:
+            return False
+        if start >= len(s):
+            return True
+        node = self.root
+        for i in range(start, len((s))):
+            if s[i] not in node.map:
+                return False
+            node = node.map[s[i]]
+            if node.isEnd:
+                if self.search(s, i+1):
+                    return True
+                self.failed[i+1] = True
+        return False
+
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-         
-        dp = [False] * (len(s)+1)
-        dp[len(s)] = True
-
-        for i in range(len(s)-1, -1, -1):
-            for word in wordDict:
-                if (i + len(word)) <= len(s) and s[i: i+len(word)] == word:
-                    dp[i] = dp[i+len(word)]
-                if dp[i]:
-                    break
-        return dp[0]
-                
-            
-        
-
-
+        trie = Trie()
+        for word in wordDict:
+            trie.insert(word)
+        return trie.search(s, 0)
